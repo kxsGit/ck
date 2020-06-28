007cd3c1a7e1a16feedf3d3d695251d15ff01309
974010252@qq.com <974010252happy>
2020-06-28 14:03:27 +0800

    tuserDao

---
+++ src/main/java/com/example/demo/dao/TUserDao.java
@@ -0,0 +1,19 @@
+package com.example.demo.dao;
+
+import com.example.demo.entity.TUser;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+import java.util.Map;
+
+/**
+ * <p>
+  *  Mapper 接口
+ * </p>
+ *
+ * @author kx
+ * @since 2019-11-08
+ */
+public interface TUserDao extends BaseMapper<TUser> {
+    Map queryUser();
+    Map selectByCcode(String ccode);
+}
